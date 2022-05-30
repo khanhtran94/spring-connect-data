bdabd3d1cee20b949263cbc864597a6545f80254
khanhTran <khanhtrantung190894@gmail.com>
2022-05-30 23:20:16 +0700

    add annotation

--- src/main/java/com/example/taco2/entity/TacoOrder.java
+++ src/main/java/com/example/taco2/entity/TacoOrder.java
@@ -1,12 +1,18 @@
 package com.example.taco2.entity;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.io.Serializable;
 import java.util.*;
 
 @Data
+@Table
 public class TacoOrder implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
     private Long id;
     private String deliveryName;
     private String deliveryStreet;
