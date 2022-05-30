6c42c4b08c96d418df906d47f96278548ca960f2
khanhTran <khanhtrantung190894@gmail.com>
2022-05-30 23:41:35 +0700

    load data with command runner

--- src/main/java/com/example/taco2/Taco2Application.java
+++ src/main/java/com/example/taco2/Taco2Application.java
@@ -1,7 +1,13 @@
 package com.example.taco2;
 
+import com.example.taco2.data.IngredientRepository;
+import com.example.taco2.entity.Ingredient;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import com.example.taco2.entity.Ingredient.Type;
 
 @SpringBootApplication
 public class Taco2Application {
@@ -9,5 +15,34 @@ public class Taco2Application {
     public static void main(String[] args) {
         SpringApplication.run(Taco2Application.class, args);
     }
-
+    @Bean
+    public CommandLineRunner dataLoader(IngredientRepository repo) {
+        return args -> {
+            repo.save(new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
+            repo.save(new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP));
+            repo.save(new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
+            repo.save(new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN));
+            repo.save(new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES));
+            repo.save(new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES));
+            repo.save(new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE));
+            repo.save(new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE));
+            repo.save(new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE));
+            repo.save(new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE));
+        };
+    }
+//    @Bean
+//    public ApplicationRunner dataLoaderApp(IngredientRepository repo) {
+//        return args -> {
+//            repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+//            repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+//            repo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+//            repo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+//            repo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+//            repo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+//            repo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
+//            repo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+//            repo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
+//            repo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+//        };
+//    }
 }
--- src/main/java/com/example/taco2/controller/OrderController.java
+++ src/main/java/com/example/taco2/controller/OrderController.java
@@ -1,4 +1,33 @@
 package com.example.taco2.controller;
 
+import com.example.taco2.data.OrderRepository;
+import com.example.taco2.entity.TacoOrder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
 public class OrderController {
+    private OrderRepository orderRepo;
+
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order, Errors errors, SessionStatus sessionStatus) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+        orderRepo.save(order);
+        sessionStatus.setComplete();
+
+        return "redirect:/";
+    }
+
 }
