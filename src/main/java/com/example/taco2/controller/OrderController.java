25828deea5e03a09556045ab6eb32d9d75d8a197
khanhTran <khanhtrantung190894@gmail.com>
2022-05-30 23:22:35 +0700

    add table for taco, controller

---
+++ src/main/java/com/example/taco2/controller/OrderController.java
@@ -0,0 +1,4 @@
+package com.example.taco2.controller;
+
+public class OrderController {
+}
--- src/main/java/com/example/taco2/entity/Taco.java
+++ src/main/java/com/example/taco2/entity/Taco.java
@@ -1,9 +1,13 @@
 package com.example.taco2.entity;
 import java.util.*;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
+@Table
 public class Taco {
+    @Id
     private Long id;
     private String name;
     private List<Ingredient> ingredients;
