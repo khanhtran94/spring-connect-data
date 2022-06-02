b89c65b33259e4b434a210b610732cad0db75bd0
khanhTran <khanhtrantung190894@gmail.com>
2022-06-03 00:24:38 +0700

    add sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -39,3 +39,43 @@ from Ingredient;
 # values ('SLSA', 'Salsa', 'SAUCE');
 # insert into Ingredient (id, name, type)
 # values ('SRCR', 'Sour Cream', 'SAUCE');
+#
+
+# gen db
+# create table ingredient
+# (
+#     id   varchar(50) null,
+#     name varchar(50) null,
+#     type varchar(50) null
+# );
+#
+# create table taco
+# (
+#     id            int auto_increment
+#         primary key,
+#     name          varchar(50) null,
+#     created_at    timestamp   null,
+#     taco_order_id int         null
+# );
+#
+# create table taco_ingredients
+# (
+#     taco_id        bigint     not null,
+#     ingredients_id varchar(4) not null
+# );
+#
+# create table taco_order
+# (
+#     id              int auto_increment
+#         primary key,
+#     delivery_name   varchar(50) null,
+#     delivery_street varchar(50) null,
+#     delivery_city   varchar(50) null,
+#     delivery_state  varchar(50) null,
+#     delivery_zip    varchar(50) null,
+#     cc_number       varchar(50) null,
+#     cc_expiration   varchar(50) null,
+#     cc_cvv          varchar(5)  null,
+#     placed_at       timestamp   null,
+#     created_at      date        null
+# );
